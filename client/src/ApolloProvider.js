1bad6c5cee2cbfbc808cff035d8adf304d5cb9b4
David-Inkheart <okoliedave1@gmail.com>
2023-06-02 23:18:19 +0100

    Get posts and display in home

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './App';
-import ApolloClient from 'apollo-client';
+import { ApolloClient } from '@apollo/client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
---
+++ client/src/components/PostCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
+import moment from "moment";
+
+function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes }}){
+
+    return (
+        <Card>
+            <Card.Content>
+                <Image
+                floated='right'
+                size='mini'
+                src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+                />
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>
+                {body}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <p>buttons here</p>
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default PostCard;
